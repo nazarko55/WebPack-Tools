e7fbc908301d9141ed8bb8e0c00c8a4b3b537018
Nazar <sikirinskijn@gmailcom>
2020-09-07 16:41:44 +0300

    Babel cofiguration

--- Lesson1/Task3/dist/profile.js
+++ Lesson1/Task3/dist/profile.js
@@ -1,7 +1,6 @@
-"use strict";
 
 var printProfile = function printProfile(profileData) {
   var name = profileData.name,
-      company = profileData.company;
+    company = profileData.company;
   console.log("".concat(name, " from ").concat(company));
 };
